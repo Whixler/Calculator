4e55ba4139caa36df1d0cef8dd8f75f1f8417a1b
Lotte <obdeijn.lotte@gmail.com>
2025-05-26 13:33:54 +0200

    show number in display

--- script.js
+++ script.js
@@ -18,4 +18,15 @@ function operation(num1, num2, operator) {
     return num1 + operator + num2;
 }
 
+const display = document.getElementById("display");
+// display.textContent = "test"
+
+
+const all_buttons = document.querySelectorAll('.button');
+
+all_buttons.forEach(bt => {
+    bt.addEventListener("click", (e) =>{
+        display.textContent = e.target.innerHTML; 
+    })
+});
 
