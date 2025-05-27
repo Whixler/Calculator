adc60f9bd2700387cdc1b02068f30bf6e2ab75fb
Lotte <obdeijn.lotte@gmail.com>
2025-05-27 09:23:28 +0200

    used eventlistener, added clear option

--- script.js
+++ script.js
@@ -19,14 +19,21 @@ function operation(num1, num2, operator) {
 }
 
 const display = document.getElementById("display");
-// display.textContent = "test"
-
+let num1 = "";
+let num2 = "";
 
 const all_buttons = document.querySelectorAll('.button');
 
 all_buttons.forEach(bt => {
     bt.addEventListener("click", (e) =>{
-        display.textContent = e.target.innerHTML; 
+        display.textContent += e.target.innerHTML; 
+        num1 += e.target.innerHTML;
+        console.log(num1);
     })
 });
 
+document.querySelector('#btnAC')?.addEventListener("click", () =>{
+    display.textContent = "";
+    num1 = "";
+    num2 = "";
+});
