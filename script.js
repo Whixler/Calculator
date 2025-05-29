f2388714bb1499548a50102c7fbeeddce08051c9
Lotte <obdeijn.lotte@gmail.com>
2025-05-29 19:59:15 +0200

    2 numbers working

--- script.js
+++ script.js
@@ -1,50 +1,61 @@
-function add(num1, num2){
-    return num1 + num2;
-}
-
-function substract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
-
-function operation(num1, num2, operator) {
-    return num1 + operator + num2;
-}
-
 const display = document.getElementById("display");
 let num1 = "";
-let num2 = "";
 let operator = "";
+let memory = ""
 
 const opbtn = document.querySelectorAll('.opbtn');
 const all_buttons = document.querySelectorAll('.button');
 
-all_buttons.forEach(bt => {
-    bt.addEventListener("click", (e) =>{
-        display.textContent += e.target.innerHTML; 
-        num1 += e.target.innerHTML;
-        console.log(num1);
-    })
-});
+if(operator === ""){
+    all_buttons.forEach(bt => {
+        bt.addEventListener("click", (e) =>{
+            display.textContent += e.target.innerHTML; 
+            num1 += e.target.innerHTML;
+            console.log({num1});
+        })
+    });
+}
+
 
 opbtn.forEach(bt => {
     bt.addEventListener("click", (e) =>{
         operator = e.target.innerHTML;
-        console.log(operator);
+        console.log({operator});
         display.textContent = "";
+        operation();
     })
 });
 
 document.querySelector('#btnAC')?.addEventListener("click", () =>{
     display.textContent = "";
     num1 = "";
-    num2 = "";
+    memory = "";
 });
 
+function operate(num1,memory){
+    if(operator === "/"){
+        if(num1 == 0){
+        display.textContent = "ERROR"
+        }
+    display.textContent = memory / num1;
+    } else if(operator === "*"){
+    display.textContent = memory * num1;
+    } else if(operator === "-"){
+    display.textContent = memory - num1;
+    } else if(operator === "+"){
+    display.textContent = memory + num1;
+    }
+}
+
+document.querySelector('#btnEqual')?.addEventListener("click", () =>{
+    console.log({num1})
+    console.log({operator})
+    console.log({memory})
+    operate(num1,memory);
+});
+
+
+function operation() {
+    memory = num1;
+    num1 = "";
+}
